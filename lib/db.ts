3ff20c99f30b1d022d79e150048a50018ebbd37e
tmrxdev <83855262+tmrxdev@users.noreply.github.com>
2026-06-21 17:27:04 +0200

    Co-authored-by: Andrei K <andrei.k@polarnl.org>

--- lib/db.ts
+++ lib/db.ts
@@ -1,24 +1,34 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@/prisma/client";
 
 const connectionString = process.env.DATABASE_URL!;
 
 const adapter = new PrismaPg({ connectionString,  });
+
 const prisma = new PrismaClient({
   adapter,
-  log: [
-    { level: "query", emit: "event" },
-    { level: "info", emit: "stdout" },
-    { level: "warn", emit: "stdout" },
-    { level: "error", emit: "stdout" },
-  ],
+  log:
+    process.env.NODE_ENV === "production"
+      ? [
+          { level: "warn", emit: "stdout" },
+          { level: "error", emit: "stdout" },
+        ]
+      : [
+          { level: "query", emit: "event" },
+          { level: "info", emit: "stdout" },
+          { level: "warn", emit: "stdout" },
+          { level: "error", emit: "stdout" },
+        ],
 });
 
-prisma.$on("query", (e: { params: string | any[]; query: any; duration: any; }) => {
-  const params = e.params.length > 64 ? `${e.params.slice(0, 64)}…` : e.params;
-  console.log(`  │  Query: ${e.query}`);
-  console.log(`  │  Params: ${params}  ${e.duration}ms`);
-});
+if (process.env.NODE_ENV !== "production") {
+  prisma.$on("query", (e) => {
+    const params =
+      e.params.length > 64 ? `${e.params.slice(0, 64)}â€¦` : e.params;
+    console.log(`  │  Query: ${e.query}`);
+    console.log(`  │  Params: ${params}  ${e.duration}ms`);
+  });
+}
 
 export { prisma };
